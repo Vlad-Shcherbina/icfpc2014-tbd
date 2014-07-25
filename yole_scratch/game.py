b93766543b4ad775f1946490fb9c5e4492f4f8f7
Dmitry Jemerov <intelliyole@gmail.com>
2014-07-25 16:06:50 +0200

    scoring and game rules

--- yole_scratch/game.py
+++ yole_scratch/game.py
@@ -2,6 +2,7 @@ UP = 0
 RIGHT = 1
 DOWN = 2
 LEFT = 3
+OPPOSITE_DIRECTIONS = [DOWN, LEFT, UP, RIGHT]
 
 STANDARD = 0
 FRIGHT = 1
@@ -23,6 +24,14 @@ LAMBDAMAN_EATING_SPEED = 137
 GHOST_SPEEDS = [130, 132, 134, 136]
 GHOST_FRIGHT_SPEEDS = [195, 198, 201, 204]
 
+FRUIT_SPAWN_TIMES = [127*200, 127*400]
+
+PILL_SCORE = 10
+POWER_PILL_SCORE = 50
+GHOSTS_EATEN_SCORES = [200, 400, 800, 1600]
+FRUIT_SCORES = [100, 300, 500, 500, 700, 700, 1000, 1000, 2000, 2000, 3000, 3000]
+
+FRIGHT_DURATION = 127 * 20
 
 class GhostAI:
     def __init__(self, code):
@@ -33,18 +42,52 @@ class GhostAI:
         pass
 
 
-class LambdaMan:
-    def __init__(self, x, y):
+class Actor(object):
+    def __init__(self, map, x, y):
+        self.map = map
         self.x = x
         self.y = y
+        self.start_x = x
+        self.start_y = y
+
+    def reset(self):
+        self.x = self.start_x
+        self.y = self.start_y
+
+
+class LambdaMan(Actor):
+    def __init__(self, map, x, y):
+        super(LambdaMan, self).__init__(map, x, y)
         self.speed = LAMBDAMAN_SPEED
+        self.score = 0
+        self.lives = 3
 
     def move(self):
-        pass
+        self.check_collisions()
+
+    def check_collisions(self):
+        c = self.map.at(self.x, self.y)
+        if c == PILL:
+            self.eat(PILL_SCORE)
+        elif c == POWER_PILL:
+            self.eat(POWER_PILL_SCORE)
+            self.map.frighten_ghosts()
+        elif c == FRUIT:
+            self.eat(self.map.fruit_score())
+
+    def eat(self, score):
+        self.map.clear(self.x, self.y)
+        self.score += score
+        self.speed = LAMBDAMAN_EATING_SPEED
+
+    def eaten(self):
+        self.lives -= 1
+        self.reset()
 
 
-class Ghost:
+class Ghost(Actor):
     def __init__(self, map, index, ai, ai_index, x, y):
+        super(Ghost, self).__init__(map, x, y)
         self.map = map
         self.index = index
         self.direction = DOWN
@@ -58,6 +101,24 @@ class Ghost:
     def move(self):
         pass
 
+    def frighten(self):
+        self.vitality = FRIGHT
+        self.direction = OPPOSITE_DIRECTIONS[self.direction]
+
+    def eaten(self):
+        self.vitality = INVISIBLE
+        self.direction = DOWN
+        self.reset()
+
+
+class FruitSpawnpoint(Actor):
+    def __init__(self, map, x, y, index):
+        super(FruitSpawnpoint, self).__init__(map, x, y)
+        self.speed = FRUIT_SPAWN_TIMES[index]
+
+    def move(self):
+        pass
+
 
 class Map:
     def __init__(self, lines, ghost_ais):
@@ -66,12 +127,16 @@ class Map:
         self.cells = []
         self.current_tick = 0
         self.move_queue = []
+        self.pills_remaining = 0
+        self.ghosts_eaten = 0
+        self.fruits = 0
+        self.fright_end = None
         for y, line in enumerate(lines):
             line_cells = []
             for x, c in enumerate(line):
                 contents = MAP_TILES.index(c)
                 if contents == LAMBDAMAN:
-                    lman = LambdaMan(x, y)
+                    lman = LambdaMan(self, x, y)
                     self.lambdamen.append(lman)
                     self.schedule(lman)
                 elif contents == GHOST:
@@ -81,11 +146,24 @@ class Map:
                     ghost = Ghost(self, index, ai_index, ai, x, y)
                     self.ghosts.append(ghost)
                     self.schedule(ghost)
+                elif contents == PILL:
+                    self.pills_remaining += 1
+                elif contents == FRUIT:
+                    spawnpoint = FruitSpawnpoint(map, x, y, self.fruits)
+                    self.fruits += 1
+                    self.schedule(spawnpoint)
+                    contents = EMPTY
                 line_cells.append(contents)
             self.cells.append(line_cells)
 
     def at(self, x, y):
         return self.cells[y][x]
+
+    def clear(self, x, y):
+        c = self.at(x, y)
+        if c == PILL:
+            self.pills_remaining -= 1
+        self.cells[y][x] = EMPTY
 
     def schedule(self, actor):
         actor.next_move = self.current_tick + actor.speed
@@ -97,5 +175,46 @@ class Map:
     def step(self):
         actor = self.move_queue.pop()
         self.current_tick = actor.next_move
+        if self.fright_end and self.current_tick >= self.fright_end:
+            self.unfrighten_ghosts()
         actor.move()
+        self.check_lman_ghost_collisions()
         self.schedule(actor)
+
+    def frighten_ghosts(self):
+        for ghost in self.ghosts:
+            ghost.frighten()
+        self.ghosts_eaten = 0
+        self.fright_end = self.current_tick + FRIGHT_DURATION
+
+    def unfrighten_ghosts(self):
+        for ghost in self.ghosts:
+            ghost.vitality = STANDARD
+        self.fright_end = None
+
+    def fruit_score(self):
+        l = self.level()
+        if l >= len(FRUIT_SCORES):
+            return FRUIT_SCORES[-1]
+        return FRUIT_SCORES[l]
+
+    def level(self):
+        return len(self.cells) * len(self.cells[0]) / 100
+
+    def check_lman_ghost_collisions(self):
+        for lman in self.lambdamen:
+            for ghost in self.ghosts:
+                if lman.x == ghost.x and lman.y == ghost.y:
+                    if ghost.vitality == FRIGHT:
+                        ghost.eaten()
+                        lman.score += self.ghost_eaten_score()
+                        self.ghosts_eaten += 1
+                    elif ghost.vitality == STANDARD:
+                        lman.eaten()
+                        for ghost in self.ghosts:
+                            ghost.reset()
+
+    def ghost_eaten_score(self):
+        if self.ghosts_eaten >= len(GHOSTS_EATEN_SCORES):
+            return GHOSTS_EATEN_SCORES[-1]
+        return GHOSTS_EATEN_SCORES[self.ghosts_eaten]
