77a0f6f31fe0099dcbaccd23e2ab999a89805f81
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2014-07-25 23:57:24 +0200

    smaller test for tournament (to speed things up) #6

--- production/tournament_tests.py
+++ production/tournament_tests.py
@@ -3,15 +3,15 @@ import tournament
 def play_tournament_test():
     results = tournament.play_tournament(
         maps=[
-            'default_map.txt',
+            #'default_map.txt',
             'gen/hz.txt',
         ],
         lm_specs=[
             'py:lm_ai.Oscillating(frequency=5)',
         ],
         ghost_team_specs=[
-            ['py:GhostAI_Shortest', 'ghc:fickle.ghc'],
+            #['py:GhostAI_Shortest', 'ghc:fickle.ghc'],
             ['ghc:miner.ghc'],
         ])
-    assert len(results) == 4
+    assert len(results) == 1
 
