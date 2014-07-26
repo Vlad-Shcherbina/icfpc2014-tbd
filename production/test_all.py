1592f9191caba499631d588d8dae849692053d75
Fj <fj.mail@gmail.com>
2014-07-26 04:38:13 +0300

    ignore visualizer.py in test_all on Windows

--- production/test_all.py
+++ production/test_all.py
@@ -1,11 +1,15 @@
-import sys
+import sys, os
 
 import nose
 
 
 if __name__ == '__main__':
-    nose.run_exit(argv=sys.argv + [
+    argv = sys.argv + [
         '--verbose', '--with-doctest',
         #'--with-coverage', '--cover-package=production',
         '--logging-level=DEBUG'
-        ])
+        ]
+    if os.name == 'nt':
+        # there's no curses on Windows
+        argv.append('--ignore-files=^visualizer.py$')
+    nose.run_exit(argv=argv)
