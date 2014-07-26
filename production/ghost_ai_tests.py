05fd4f591dd7061cc376f5340011f5368c32a552
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2014-07-27 01:38:46 +0200

    test for hunter ghost

--- production/ghost_ai_tests.py
+++ production/ghost_ai_tests.py
@@ -11,3 +11,16 @@ def test_original_strategies():
         ghost_team_specs=[
             ['py:GhostAI_Red', 'py:GhostAI_Pink'],
         ])
+
+def test_hunter():
+    tournament.play_tournament(
+        maps=[
+            'default_map.txt',
+        ],
+        lm_specs=[
+            'py:lm_ai.NearestPill()',
+            'py:lm_ai.NearestPill(straight=True)',
+        ],
+        ghost_team_specs=[
+            ['py:Hunter'],
+        ])
