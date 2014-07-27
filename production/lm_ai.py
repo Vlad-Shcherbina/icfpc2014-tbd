ec1c054ca0b6b668dd9f822fe7c6b82fd6de2c2b
Dmitry Jemerov <intelliyole@gmail.com>
2014-07-27 17:40:24 +0200

    humanoid AI

--- production/lm_ai.py
+++ production/lm_ai.py
@@ -116,4 +116,103 @@ class TunnelDigger(game.LambdaManAI):
         return self.rng.choice(game.DIRECTIONS)
 
 
+class Humanoid(game.LambdaManAI):
+    """ By default, continue moving in the same direction (following bends),
+        unless we are being chased by a ghost, in which case flee. At a
+        junction, choose to move in the direction where we see the most number
+        of pills/fruit in the straight line leading from the junction. """
+    def get_move(self, map):
+        open_directions = self.get_open_directions(map)
+        if not open_directions:
+            raise Exception("ZAMUROVALI DEMONY!")
 
+        if len(open_directions) == 1:
+            return open_directions[0]
+
+        if not self.ghosts_frightened(map):
+            safe_directions = [
+                d for d in open_directions
+                if self.distance_to_ghost(map, d) == map.width()]
+        else:
+            safe_directions = open_directions
+
+        if len(safe_directions) == 1:
+            return safe_directions[0]
+
+        if len(open_directions) == 2:
+            # If we have just two possible directions, prefer to follow the path
+            # rather than turn around.
+            if map.lambdaman.direction == game.OPPOSITE_DIRECTIONS[open_directions[0]]:
+                return open_directions[1]
+            else:
+                return open_directions[0]
+
+        max_value = 0
+        max_net_value = 0
+        best_direction = open_directions[0]
+        best_net_direction = open_directions[0]
+        for d in open_directions:
+            value, penalty = self.expected_value(map, d)
+            if value > max_value:
+                best_direction = d
+                max_value = value
+            if value - penalty > max_net_value:
+                best_net_direction = d
+                max_net_value = value - penalty
+        if max_net_value > 0:
+            return best_net_direction
+        return best_direction
+
+    def get_open_directions(self, map):
+        result = []
+        for direction in range(4):
+            cell = map.at(map.lambdaman.x + game.DELTA_X[direction],
+                          map.lambdaman.y + game.DELTA_Y[direction])
+            if cell:
+                result.append(direction)
+        return result
+
+    def ghosts_frightened(self, map):
+        return map.ghosts[0].vitality == game.FRIGHT
+
+    def ghost_at(self, map, x, y):
+        return any(g for g in map.ghosts if g.x == x and g.y == y)
+
+    def distance_to_ghost(self, map, direction):
+        for x, y, distance in self.iterate_cells_until_wall(map, direction):
+            if self.ghost_at(map, x, y):
+                return distance
+        return map.width()
+
+    def iterate_cells_until_wall(self, map, direction):
+        dx = game.DELTA_X[direction]
+        dy = game.DELTA_Y[direction]
+        x = map.lambdaman.x
+        y = map.lambdaman.y
+        distance = 0
+        while True:
+            x += dx
+            y += dy
+            distance += 1
+            if x < 0 or x >= map.width() or y < 0 or y >= map.height():
+                break
+            if map.at(x, y) == game.WALL:
+                break
+            yield x, y, distance
+
+    def expected_value(self, map, direction):
+        result = 0
+        penalty = 0
+        for x, y, distance in self.iterate_cells_until_wall(map, direction):
+            cell = map.at(x, y)
+            if cell == game.PILL:
+                result += 10
+            elif cell == game.POWER_PILL:
+                result += 50
+            elif cell == game.FRUIT:
+                result += 100
+            elif cell == game.EMPTY and result <= 0:
+                # Penalize empty cells that we need to go through in order to
+                # reach the pills.
+                penalty += 10
+        return result, penalty
