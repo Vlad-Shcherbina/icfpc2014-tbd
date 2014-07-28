3f000000825e638e230d04b45507a310ec25766c
Aleksejs Popovs <me@popoffka.ru>
2014-07-28 13:13:35 +0300

    updated the packing script

--- pack.sh
+++ pack.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-FILENAME="lightning_tbd.tar.gz"
+FILENAME="icfpc_tbd.tar.gz"
 
 mkdir -p submission/solution
+mkdir submission/code
 
-cp data/lms/lightning.gcc submission/solution/lambdaman.gcc
+# solutions
+cp data/lms/ff.gcc		submission/solution/lambdaman.gcc
+cp data/ghosts/redsplitt.ghc	submission/solution/ghost0.ghc
 
-cp -r production            submission/code
-cp vlad_scratch/gen_gcc.py  submission/code/
-cp yole_scratch/gcc_ast.py  submission/code/
-cp README                   submission/code/
-# cp -r *_scratch submission/code
+# code
+cp -r *_scratch			submission/code/
+cp -r production		submission/code/
+cp -r data			submission/code/
+cp README			submission/code/
 
-tar -czf $FILENAME submission/*
-rm -rf submission
+# removing some crap from data
+rm -f submission/code/data/id_rsa*
+rm -f submission/code/some_results.json
+
+tar -czf $FILENAME -C submission solution code
+#rm -rf submission
 
 echo "saved submission in $FILENAME"
 sha1sum $FILENAME
 echo "secret token for submission: 69ce5cb1-66e0-4a5b-b1dd-ac97269b74e3"
 
 echo "will now upload file"
-curl --form "file=@$FILENAME" "http://f.nn.lv/?f=rest"
+#curl --form "file=@$FILENAME" "http://f.nn.lv/?f=rest"
 echo ""
